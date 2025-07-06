7c06979f06aae7687a71b2780b0c2a815c048946
Erik Lundevall-Zara <45368867+eriklz@users.noreply.github.com>
2025-07-06 12:43:49 +0200

    fix: update scripts/ShowServers.fsx to match update to part 2 of F3 for the Cloud Worker

--- scripts/ShowServers.fsx
+++ scripts/ShowServers.fsx
@@ -5,7 +5,6 @@
 #r "nuget: AWSSDK.EC2"
 
 open System
-open System.Environment
 open Amazon.EC2
 open Amazon.EC2.Model
 
@@ -23,15 +22,13 @@ type InstanceInfo =
       Name:               string
       InstanceType:       string
       PrivateHostAddress: IpAddressOrDns
-      LaunchTime:         DateTime
+      LaunchTime:         DateTime option
       State:              string }
-
 
 let rec getTagValue key (tags: Tag list) =
     match tags with
     | [] -> ""
     | head :: tail -> if head.Key = key then head.Value  else getTagValue key tail
-
 
 let getInstanceInfo (instance: Instance) =
     let tags = List.ofSeq instance.Tags
@@ -40,61 +37,51 @@ let getInstanceInfo (instance: Instance) =
         Name = getTagValue "Name" tags
         InstanceType = instance.InstanceType.Value
         PrivateHostAddress = IpAddress instance.PrivateIpAddress
-        LaunchTime = instance.LaunchTime
+        LaunchTime = if instance.LaunchTime.HasValue then Some instance.LaunchTime.Value else None
         State = instance.State.Name.Value
     }
 
-
-let yyyymmddhhmmss (dt: DateTime) =
-    dt.ToString ("yyyy-MM-dd HH:mm:ss")
-
+let yyyymmddhhmmss (dt: DateTime option) =
+    match dt with
+      | Some x -> x.ToString "yyyy-MM-dd HH:mm:ss"
+      | None -> "not launched"
 
 let printInstanceInfo (instanceInfos: InstanceInfo list) : unit =
     if instanceInfos.IsEmpty then
         printfn "No instance info available!"
     else
-        printfn  "%-20s %-20s %-16s %-21s %-20s %-10s"
-            "InstanceId" "Name" "InstanceType" "Private Host Address" "LaunchTime" "State"
+        printfn "%-20s %-20s %-16s %-16s %-20s %-10s" 
+            "InstanceId" "Name" "InstanceType" "Private IP" "LaunchTime" "State"
         for ii in instanceInfos do
-            printfn "%-20s %-20s %-16s %-21s %-20s %-10s"
+            printfn "%-20s %-20s %-16s %-16s %-20s %-10s"
                 ii.InstanceId ii.Name ii.InstanceType (toString ii.PrivateHostAddress) (yyyymmddhhmmss ii.LaunchTime) ii.State
-
 
 let getInstances (reservations: Reservation list) =
     reservations |> List.collect (fun x -> List.ofSeq x.Instances)
 
 
 let describeInstances (client: AmazonEC2Client) =
-    async {
-        let! response = client.DescribeInstancesAsync() |> Async.AwaitTask
-        return response
+    task {
+        return! client.DescribeInstancesAsync()
     }
-    
+
 
 let showServers (args: string[]) =
     if args.Length > 0 then
-        do SetEnvironmentVariable ("AWS_PROFILE", args.[0])
+        do Environment.SetEnvironmentVariable ("AWS_PROFILE", args.[0])
     let client = new AmazonEC2Client()
-    let response = (describeInstances client) |> Async.RunSynchronously
-    let infolist = 
-        if response.Reservations.Count = 0 then
-            List.empty<Instance>
-        else
-            (List.ofSeq response.Reservations) |> getInstances
-    infolist |> List.map getInstanceInfo |> printInstanceInfo
+    task {
+        let! response = describeInstances client
+        let infoList = 
+            if response.Reservations.Count = 0 then
+                List.empty<Instance>
+            else
+                List.ofSeq response.Reservations |> getInstances
+        infoList
+            |> List.map getInstanceInfo
+            |> printInstanceInfo
+    } |> Async.AwaitTask |> Async.RunSynchronously
 
-    
+
 showServers (fsi.CommandLineArgs |> Array.skip 1)
 
-(*
-  showServers [| "erik" |]
-  GetEnvironmentVariable("AWS_PROFILE")
-  let mylist = List.ofSeq response.Reservations
-  let response = showServers [| "erik" |]
-  let iis = showServers [| "erik" |] |> List.map getInstanceInfo
-  showServers [| "erik" |] |> List.map getInstanceInfo |> printInstanceInfo 
-  getInstanceInfo response.[0]
-  let instance = response.[0]
-  let tags = List.ofSeq instance.Tags
-  getTagValue "Name" tags
-*)
